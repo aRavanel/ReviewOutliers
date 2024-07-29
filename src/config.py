0a45c54551e869610aecdac018cc5848369eccfc
aRavanel <alexis_ravanel@hotmail.fr>
2024-07-29 16:43:28 +1100

    outlier path modifications

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 # load model from sentence transformers
-MODEL_NAME_EMBEDDINGS = "all-MiniLM-L6-v2"  # dim : 384, max_len : 256
+MODEL_NAME_EMBEDDINGS = (
+    "all-MiniLM-L6-v2"  # dim : 384, max_len : 256  - A good balance between performance and dimensionality
+)
 
 # outlier model name
 MODEL_NAME = "isolation_forest"  #
