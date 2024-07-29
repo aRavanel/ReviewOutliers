22e46390d9c009c3406282b828652e9caa2355df
aRavanel <alexis_ravanel@hotmail.fr>
2024-07-30 10:24:22 +1100

    preprocessing refacto

--- src/utils/preprocessing/preprocessing_cleaning.py
+++ src/utils/preprocessing/preprocessing_cleaning.py
@@ -6,97 +6,90 @@ import pandas as pd
 # ==========================================================================
 
 
+def clean_enrich(df_in: pd.DataFrame) -> pd.DataFrame:
+    """ """
+    df = df_in.copy()
+
+    # Expand timestamp
+    df["timestamp_review"] = pd.to_datetime(df["timestamp_review"], unit="ms")
+    df["year_review"] = df["timestamp_review"].dt.year
+    df["month_review"] = df["timestamp_review"].dt.month
+    df["day_review"] = df["timestamp_review"].dt.day
+    df["hour_review"] = df["timestamp_review"].dt.hour
+
+    # Handle missing values - categories
+    df["main_category_metadata"] = df["main_category_metadata"].fillna("unknown").astype("category")
+    df["store_metadata"] = df["store_metadata"].fillna("unknown").astype("category")
+
+    # Handle missing values - str
+    df["title_review"] = df["title_review"].fillna("").astype("str")
+    df["title_metadata"] = df["title_metadata"].fillna("").astype("str")
+    df["text_review"] = df["text_review"].fillna("").astype("str")
+    df["user_id_review"] = df["user_id_review"].fillna("").astype("str")
+    df["features_metadata"] = (
+        df["features_metadata"].apply(lambda x: " ".join(x) if isinstance(x, list) and x else "").astype("str")
+    )
+    df["metadata_text"] = df[["title_metadata", "features_metadata"]].astype(str).agg("/n/n".join, axis=1)
+    # df_metadata['description'] = df_metadata['description'].fillna('').astype("str")
+
+    # Handle missing values - numeric
+    numeric_fields = {
+        "year_review",
+        "month_review",
+        "day_review",
+        "hour_review",
+        "rating_review",
+        "helpful_vote_review",
+        "verified_purchase_review",
+        "rating_number_metadata",
+    }
+    for field in numeric_fields:
+        df[field] = df[field].fillna(-1).astype(int)
+    df["average_rating_metadata"] = df["average_rating_metadata"].fillna(-1).astype("float")
+    # df_metadata['price'] = df_metadata['price'].fillna(-1).astype("float")
+
+    # concatenate some text together
+    # -> this way less features because each embeddings is of high dimensionality
+    df["review_text"] = df["title"] + "/n/n" + df["text"]
+    df["metadata_text"] = df[["title", "features"]].astype(str).agg("/n/n".join, axis=1)
+
+    # Drop features that are too complex, not informative, or have been transformed
+    features_to_drop = [
+        "timestamp_review",  # processed
+        "title_review",  # processed
+        "text_review",  # processed
+        "title_metadata",  # processed
+        "features_metadata",  # processed
+        "user_id_review",  #
+        "images_review",  # too complex
+        "images_metadata",  # too complex
+        "videos_metadata",  # too complex
+        "details_metadata",  # mostly empty
+        "categories_metadata",  # mostly empty
+        "bought_together_metadata",  # mostly empty
+        "price_metadata",  # mostly empty
+        "description_metadata",  # mostly empty
+    ]
+    df.drop(columns=features_to_drop, inplace=True)
+
+    # Drop duplicates and rows with missing 'asin' or 'parent_asin'
+    df.drop_duplicates(inplace=True)
+    df = df.dropna(subset=["asin", "parent_asin"])
+
+    return df
+
+
 # ==========================================================================
 # Exported functions
 # ==========================================================================
 
 
-def clean_enrich_reviews(df_review_in: pd.DataFrame) -> pd.DataFrame:
-    """
-    Clean and enrich the reviews DataFrame.
-
-    Parameters:
-    - df_review_in (pd.DataFrame): Input DataFrame containing review data.
-
-    Returns:
-    - pd.DataFrame: Cleaned and enriched DataFrame.
-    """
-
-    df_review = df_review_in.copy()
-
-    # Expand timestamp
-    df_review["timestamp"] = pd.to_datetime(df_review["timestamp"], unit="ms")  # Convert Unix timestamp to datetime
-    df_review["year"] = df_review["timestamp"].dt.year
-    df_review["month"] = df_review["timestamp"].dt.month
-    df_review["day"] = df_review["timestamp"].dt.day
-    df_review["hour"] = df_review["timestamp"].dt.hour
-
-    # Handle missing values (ugly, to fix later)
-    # -> if str fill with empty string
-    # -> if numeric or categorical: fill with a dummy value
-    df_review["year"] = df_review["year"].fillna(-1).astype("int")
-    df_review["month"] = df_review["month"].fillna(-1).astype("int")
-    df_review["day"] = df_review["day"].fillna(-1).astype("int")
-    df_review["hour"] = df_review["hour"].fillna(-1).astype("int")
-    df_review["rating"] = df_review["rating"].fillna(-1).astype("int")
-    df_review["title"] = df_review["title"].fillna("").astype("str")
-    df_review["text"] = df_review["text"].fillna("").astype("str")
-    df_review["user_id"] = df_review["user_id"].fillna("").astype("str")
-    df_review["helpful_vote"] = df_review["helpful_vote"].fillna(-1).astype("int")
-    df_review["verified_purchase"] = df_review["verified_purchase"].fillna(-1).astype("int")
-
-    # Concatenate title and text to form review_text
-    # -> this way less features because each embeddings is of high dimensionality
-    df_review["review_text"] = df_review["title"] + "/n/n" + df_review["text"]
-
-    # drop some features:
-    features_drop = ["timestamp", "text", "title"]  # transformed
-    features_drop += ["images", "user_id"]  # too complex or not informative
-    df_review = df_review.drop(columns=features_drop)
-
-    # Drop duplicates and rows with missing 'asin' or 'parent_asin'
-    df_review.drop_duplicates(inplace=True)
-    df_review = df_review.dropna(subset=["asin", "parent_asin"])
-
-    return df_review
-
-
-def clean_enrich_metadata(df_metadata_in: pd.DataFrame) -> pd.DataFrame:
+def clean_data(df_review, df_metadata) -> pd.DataFrame:
     """ """
-    df_metadata = df_metadata_in.copy()
+    # merge data
+    df_merged = pd.merge(df_review, df_metadata, on="parent_asin", how="inner", suffixes=("_review", "_metadata"))
 
-    # Handle missing values
-    # features -> concatenated in a string
-    df_metadata["main_category"] = df_metadata["main_category"].fillna("").astype("category")
-    df_metadata["title"] = df_metadata["title"].fillna("").astype("str")
-    df_metadata["average_rating"] = df_metadata["average_rating"].fillna(-1).astype("float")
-    df_metadata["rating_number"] = df_metadata["rating_number"].fillna(-1).astype("int")
-    df_metadata["features"] = (
-        df_metadata["features"].apply(lambda x: " ".join(x) if isinstance(x, list) and x else "").astype("str")
-    )
-    df_metadata["store"] = df_metadata["store"].fillna("").astype("category")
-    df_metadata["parent_asin"] = df_metadata["parent_asin"].fillna("").astype("str")
-    # df_metadata['description'] = df_metadata['description'].fillna('')
-    # df_metadata['price'] = df_metadata['price'].fillna(0)
+    # clean it
+    df_merged = clean_enrich(df_merged)
 
-    # concatenate some text together
-    # -> this way less features because each embeddings is of high dimensionality
-    df_metadata["metadata_text"] = df_metadata[["title", "features"]].astype(str).agg("/n/n".join, axis=1)
-
-    # Drop some features
-    # - `images` (list of str): too complex
-    # - `videos` (list of str): too complex
-    # - `details` (dict): mostly empty
-    # - `categories`: mostly empty
-    # - `bought_together` (boolean): mostly empty
-    # - `price` (float): mostly empty
-    # - `description` (list of str): mostly empty
-    features_to_drop = ["title", "features"]
-    features_to_drop += ["images", "videos", "details", "categories", "bought_together", "price", "description"]
-    df_metadata = df_metadata.drop(columns=features_to_drop)
-
-    # Drop samples
-    df_metadata.drop_duplicates(inplace=True)
-    df_metadata = df_metadata.dropna(subset=["parent_asin"])  # Drop rows if 'asin' or 'parent_asin' are not filled
-
-    return df_metadata
+    return df_merged
